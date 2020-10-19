e2c64ca7d8fdfc150c5b0f3d14ba3b135dc80046
Real Coin <72079738+Realcoinco@users.noreply.github.com>
2020-10-19 02:39:22 -0700

    uplaod

---
+++ contracts/truffle.js
@@ -0,0 +1,48 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const mnemonic = process.env.TEST_MNEMONIC || 'real mnemonic real mnemonic real mnemonic real mnemonic real mnemonic real mnemonic';
+const providerRopsten = new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/', 0);
+const providerKovan = new HDWalletProvider(mnemonic, 'https://kovan.infura.io', 0);
+
+module.exports = {
+    networks: {
+        development: {
+            network_id: 15,
+            host: "localhost",
+            port: 8545,
+            gas: 4000000,
+            gasPrice: 20e9,
+            from: '0x8a838b1722750ba185f189092833791adb98955f',
+        },
+        development_migrate: {
+            network_id: 15,
+            host: "localhost",
+            port: 8545,
+            gas: 4000000,
+            gasPrice: 20e9,
+            from: "0x0581E7aF436e9380a8B772885A66Bd84F790939D",
+        },
+        mainnet: {
+            network_id: 1,
+            host: "localhost",
+            port: 8545,
+            gas: 4000000,
+            gasPrice: 20e9,
+            from: "0x0581E7aF436e9380a8B772885A66Bd84F790939D",
+        },
+        ropsten: {
+            network_id: 3,
+            host: "localhost",
+            port: 8545,
+            from: '0x0581E7aF436e9380a8B772885A66Bd84F790939D',
+            //gas: 4000000,
+            //gasPrice: 20e9,
+        },
+        kovan: {
+            network_id: 42,
+            provider: providerKovan,
+            gas: 4000000,
+            gasPrice: 20e9,
+        },
+    }
+};
